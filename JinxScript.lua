7d3099ebdb1d188ce061187deae5cfe1d72290fe
Prism <109115519+Prisuhm@users.noreply.github.com>
2022-07-25 21:01:37 -0700

    Update JinxScript.lua

--- JinxScript.lua
+++ JinxScript.lua
@@ -5,7 +5,7 @@ local localVer = 1.4
 async_http.init("raw.githubusercontent.com", "/Prisuhm/JinxScript/main/JinxScriptVersion", function(output)
     currentVer = tonumber(output)
     if localVer ~= currentVer then
-        util.toast("Outdated JinxScript Version Detected, Download Most Up-To-Date Build.")
+        util.toast("Outdated JinxScript version detected. Downloading the most recent version :)")
         async_http.init('raw.githubusercontent.com','/Prisuhm/JinxScript/main/JinxScript.lua',function(a)
             local f = io.open(filesystem.scripts_dir()..SCRIPT_RELPATH, "wb")
             f:write(a)

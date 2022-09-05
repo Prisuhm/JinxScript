563fe0c83693d79d3f93a1f8bdd82edd2ed0ac31
Prism <109115519+Prisuhm@users.noreply.github.com>
2022-09-04 20:16:04 -0700

    Update JinxScript.lua

--- JinxScript.lua
+++ JinxScript.lua
@@ -1,7 +1,7 @@
 util.require_natives("natives-1660775568-uno")
 util.toast("Welcome To JinxScript!\n" .. "Official Discord: https://discord.gg/hjs5S93kQv") 
 local response = false
-local localVer = 2.23
+local localVer = 2.24
 async_http.init("raw.githubusercontent.com", "/Prisuhm/JinxScript/main/JinxScriptVersion", function(output)
     currentVer = tonumber(output)
     response = true
